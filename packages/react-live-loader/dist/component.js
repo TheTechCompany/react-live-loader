d3a88fdc5be8535208651339ad0bc974ccf8408c
Ross Leitch <professional.balbatross@gmail.com>
2021-06-05 23:37:07 +1200

    Timing ops

--- packages/react-live-loader/dist/component.js
+++ packages/react-live-loader/dist/component.js
@@ -1,7 +1,26 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LiveComponent = void 0;
-var react_1 = require("react");
+var react_1 = __importStar(require("react"));
 var _1 = require(".");
 var LiveComponent = function (props) {
     var _a = react_1.useState(), Component = _a[0], setComponent = _a[1];
@@ -11,6 +30,6 @@ var LiveComponent = function (props) {
             setComponent(component);
         }
     }, [props.code]);
-    return Component && Component(props.props);
+    return Component ? Component(props.props) : react_1.default.createElement(react_1.default.Fragment, null);
 };
 exports.LiveComponent = LiveComponent;
--- packages/react-live-loader/src/component.tsx
+++ packages/react-live-loader/src/component.tsx
@@ -16,5 +16,5 @@ export const LiveComponent : React.FC<LiveComponentProps> = (props) => {
         }
     }, [props.code])
 
-    return Component && Component(props.props)
+    return Component ? Component(props.props) : <></>
 }
